500cbd70ef0723aebca82dd5d4bbe183f8c3c494
bkelly16 <bkelly@45drives.com>
2019-03-21 14:54:07 -0300

    Create alias_setup.sh

---
+++ alias_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VDEV_RULES="http://images.45drives.com/ceph/udev/69-vdev.rules"
+VDEV_ID="http://images.45drives.com/ceph/udev/vdev_id"
+
+curl -o /usr/lib/udev/rules.d/69-vdev.rules $VDEV_RULES
+curl -o /usr/lib/udev/vdev_id $VDEV_ID > /dev/null ; chmod +x /usr/lib/udev/vdev_id
+touch /etc/vdev_id.conf
+
+if [ -f /usr/lib/udev/rules.d/69-vdev.rules ] && [ -x /usr/lib/udev/vdev_id ] && [ -f /etc/vdev_id.conf ];then
+	echo "Installation Successful"
+	echo "Configure alias with dmap"
+else
+	echo "Installation failed, double check network connection. Internet Access required"
+fi
+	
