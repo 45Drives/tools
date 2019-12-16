c8d51f5d1447ff0a57d7f02e11c0bf6170290cc5
bkelly16 <bkelly@45drives.com>
2019-12-16 17:38:34 -0400

    Update alias_setup.sh

--- alias_setup.sh
+++ alias_setup.sh
@@ -3,6 +3,11 @@
 VDEV_RULES="http://images.45drives.com/ceph/udev/69-vdev.rules"
 VDEV_ID="http://images.45drives.com/ceph/udev/vdev_id"
 
+if rpm -q zfs >/dev/null 2>&1;then
+	echo "ZFS is installed quiting"
+	echo "This will overwrite ZFS udev rules"
+fi
+
 curl -o /usr/lib/udev/rules.d/69-vdev.rules $VDEV_RULES
 curl -o /usr/lib/udev/vdev_id $VDEV_ID > /dev/null ; chmod +x /usr/lib/udev/vdev_id
 touch /etc/vdev_id.conf
