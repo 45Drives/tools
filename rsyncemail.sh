99bfbd3ef17d9cd94078367f5764062eaa1b7197
mitcHELLspawn <50410353+mitcHELLspawn@users.noreply.github.com>
2020-02-26 12:17:06 -0400

    Update rsyncemail.sh

--- rsyncemail.sh
+++ rsyncemail.sh
@@ -4,8 +4,8 @@
 # Preconfigure for your environment
 logfile="/tmp/rsynctasks.tmp"
 email="emailaddress@gmail.com"  # email address to send notifications to
-sourcedir="/source/path"  
-destinationdir="/destination/path"
+sourcedir="/source/path"   # source directory for Rsync task
+destinationdir="/destination/path" # Destination for Rsync task
 
  
 rsync -avz ${sourcedir} user@192.0.0.0:${destinationdir} --log-file=${logfile};   # set the user and IP address here and any additional Rsync flags you may want
