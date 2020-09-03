c70c73be198aa7c0a8650999e817a3d979211cf9
markdhooper <markushooper@gmail.com>
2020-09-03 13:17:48 -0300

    updated the build-v1_5.sh script

--- build-v1_5.sh
+++ build-v1_5.sh
@@ -1,3 +1,6 @@
+# don't run this if you have a ~/rpmbuild folder that you don't want to lose! 
+mkdir 45drives-temp
+cd 45drives-temp
 mkdir rpmbuild rpmbuild/RPMS rpmbuild/SOURCES rpmbuild/SPECS rpmbuild/SRPMS rpmbuild/RPMS/noarch
 git clone https://github.com/45Drives/tools.git
 mkdir 45drives-tools-1.5
@@ -8,7 +11,9 @@ rm -rf 45drives-tools-1.5
 mv 45drives-tools-1.5.tar.gz rpmbuild/SOURCES/45drives-tools-1.5.tar.gz
 mv tools/tools.spec rpmbuild/SPECS/tools.spec
 rm -rf tools
-cp -r rpmbuild ~/rpmbuild
-rm -rf rpmbuild
+rm -rf ~/rpmbuild
+cd ..
+cp -r 45drives-temp/rpmbuild ~/rpmbuild
+rm -rf 45drives-temp
 cd ~/rpmbuild
 rpmbuild -ba SPECS/tools.spec
