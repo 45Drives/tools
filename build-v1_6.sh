b3fa2cc74082e03d680d4c927704880aec2898a4
bkelly16 <bkelly@45drives.com>
2020-09-26 12:02:26 -0300

    added confirmation check on build script

--- build-v1_6.sh
+++ build-v1_6.sh
@@ -1,4 +1,15 @@
-# don't run this if you have a ~/rpmbuild folder that you don't want to lose! 
+echo "Don't run this if you have a ~/rpmbuild folder that you don't want to lose!"
+read -p "Are you sure you want to continue ? (yes/no) " con
+case $con in
+    yes)
+        ;;
+    *)
+        echo "exiting..."
+        exit 0
+        ;;
+esac
+
+
 mkdir 45drives-temp
 cd 45drives-temp
 mkdir rpmbuild rpmbuild/RPMS rpmbuild/SOURCES rpmbuild/SPECS rpmbuild/SRPMS
