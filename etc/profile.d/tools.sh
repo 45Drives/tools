e7cbbd8447f9f307a22b4b3c2623f64b29baa002
Brett Kelly <br991561@gmail.com>
2020-11-07 14:34:48 -0400

    update profile.d to use /dev/disk/by-alias of /dev

--- etc/profile.d/tools.sh
+++ etc/profile.d/tools.sh
@@ -2,7 +2,7 @@ if rpm -q zfs >/dev/null 2>&1;then
 	export ALIAS_DEVICE_PATH=/dev/disk/by-vdev
 	export ALIAS_CONFIG_PATH=/etc/zfs
 else
-        export ALIAS_DEVICE_PATH=/dev
+        export ALIAS_DEVICE_PATH=/dev/disk/by-alias
         export ALIAS_CONFIG_PATH=/etc
 fi
 
