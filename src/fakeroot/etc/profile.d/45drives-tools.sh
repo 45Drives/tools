7cebf02ee0d50b450158360f4eb8428ec640a6ae
markdhooper <markushooper@gmail.com>
2021-01-28 08:59:40 -0400

    added symbolic link creation to profile.d script

--- src/fakeroot/etc/profile.d/45drives-tools.sh
+++ src/fakeroot/etc/profile.d/45drives-tools.sh
@@ -6,6 +6,7 @@ else
         export ALIAS_CONFIG_PATH=/etc
 fi
 
+ln -sf /opt/45drives/tools /opt/tools
 alias lshealth="echo lshealth is deprecated. Using \'lsdev -H\'; lsdev -H"
 alias lsmodel="echo lsmodel is deprecated. Using \'lsdev -m\'; lsdev -m"
 alias lsosd="echo lsosd is deprecated. Using \'lsdev -o\'; lsdev -o"
