402d69ce3faaefd758cb9e66b9508ee232007d5e
markdhooper <markushooper@gmail.com>
2021-02-11 16:18:57 -0400

    modified profile.d script to configure ALIAS_DEVICE_PATH and ALIAS_CONFIG_PATH on both ubuntu and centos7

--- src/fakeroot/etc/profile.d/45drives-tools.sh
+++ src/fakeroot/etc/profile.d/45drives-tools.sh
@@ -1,9 +1,16 @@
-if rpm -q zfs >/dev/null 2>&1;then
-	export ALIAS_DEVICE_PATH=/dev/disk/by-vdev
-	export ALIAS_CONFIG_PATH=/etc/zfs
+#!/bin/bash
+command -v zfs >/dev/null
+zfs_installed=$?
+if [[ $zfs_installed -eq 0 ]]; then
+        unset ALIAS_DEVICE_PATH
+        unset ALIAS_CONFIG_PATH
+        export ALIAS_DEVICE_PATH="/dev/disk/by-vdev"
+        export ALIAS_CONFIG_PATH="/etc/zfs"
 else
-        export ALIAS_DEVICE_PATH=/dev
-        export ALIAS_CONFIG_PATH=/etc
+        unset ALIAS_DEVICE_PATH
+        unset ALIAS_CONFIG_PATH
+        export ALIAS_DEVICE_PATH="/dev"
+        export ALIAS_CONFIG_PATH="/etc"
 fi
 
 alias lshealth="echo lshealth is deprecated. Using \'lsdev -H\'; lsdev -H"
