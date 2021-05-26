014730b38f65681cbe1ca81590a4bd362f1fec91
bkelly16 <bkelly@45drives.com>
2021-05-26 16:22:01 -0300

    petasan build process

--- package-deb.sh
+++ package-deb.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+UBUNTU_VERSION=$1
+
+if [[ $UBUNTU_VERSION != "focal" ]] && [[ $UBUNTU_VERSION != "bionic" ]];then
+	echo "version required, focal or bionic"
+	echo "./package-deb.sh <focal|bionic>"
+	exit 1
+fi
+
 # fill this in with a name unique to the software package
 PACKAGE_NAME=45drives-tools
 
@@ -10,8 +18,8 @@ command -v docker > /dev/null 2>&1 || {
 }
 
 # if docker image DNE, build it (keep container tag name unique to software package)
-if [[ "$(docker images -q $PACKAGE_NAME-ubuntu-builder 2> /dev/null)" == "" ]]; then
-	docker build -t $PACKAGE_NAME-ubuntu-builder - < docker/ubuntu # pass in path to docker file
+if [[ "$(docker images -q $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION 2> /dev/null)" == "" ]]; then
+	docker build -t $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION - < docker/ubuntu-$UBUNTU_VERSION # pass in path to docker file
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo "Building docker image failed."
@@ -24,7 +32,7 @@ make clean
 mkdir -p dist/ubuntu
 
 # mirror current directory to working directory in container, and mirror dist/ubuntu to .. for deb output
-docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm $PACKAGE_NAME-ubuntu-builder dpkg-buildpackage -us -uc -b
+docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION dpkg-buildpackage -us -uc -b
 res=$?
 if [ $res -ne 0 ]; then
 	echo "Packaging failed."
