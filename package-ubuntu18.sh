0d203d86b468004b05bc7878c6f57c7dcc32ecbf
bkelly16 <bkelly@45drives.com>
2021-05-26 16:59:42 -0300

    update for ubuntu18 builds

--- package-ubuntu18.sh
+++ package-ubuntu18.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-UBUNTU_VERSION=$1
-
-if [[ $UBUNTU_VERSION != "focal" ]] && [[ $UBUNTU_VERSION != "bionic" ]];then
-	echo "version required, focal or bionic"
-	echo "./package-deb.sh <focal|bionic>"
-	exit 1
-fi
-
 # fill this in with a name unique to the software package
 PACKAGE_NAME=45drives-tools
 
@@ -18,8 +10,8 @@ command -v docker > /dev/null 2>&1 || {
 }
 
 # if docker image DNE, build it (keep container tag name unique to software package)
-if [[ "$(docker images -q $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION 2> /dev/null)" == "" ]]; then
-	docker build -t $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION - < docker/ubuntu-$UBUNTU_VERSION # pass in path to docker file
+if [[ "$(docker images -q $PACKAGE_NAME-ubuntu-builder-ubuntu18 2> /dev/null)" == "" ]]; then
+	docker build -t $PACKAGE_NAME-ubuntu-builder-ubuntu18 - < docker/ubuntu18 # pass in path to docker file
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo "Building docker image failed."
@@ -32,7 +24,7 @@ make clean
 mkdir -p dist/ubuntu
 
 # mirror current directory to working directory in container, and mirror dist/ubuntu to .. for deb output
-docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm $PACKAGE_NAME-ubuntu-builder-$UBUNTU_VERSION dpkg-buildpackage -us -uc -b
+docker run -u $(id -u):$(id -g) -w /home/deb/build -it -v$(pwd):/home/deb/build -v$(pwd)/dist/ubuntu:/home/deb --rm $PACKAGE_NAME-ubuntu-builder-ubuntu18 dpkg-buildpackage -us -uc -b
 res=$?
 if [ $res -ne 0 ]; then
 	echo "Packaging failed."
